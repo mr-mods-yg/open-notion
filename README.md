c475beded2d1d166d28c4fd8ccfe171b25cef7d8
Yash Garg <yg292001@gmail.com>
2025-12-18 14:03:29 +0530

    create a workspace on user signup

--- README.md
+++ README.md
@@ -102,8 +102,3 @@ Don't forget to give the project a star! Thanks again!
 3.  Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
 4.  Push to the Branch (`git push origin feature/AmazingFeature`)
 5.  Open a Pull Request
-
-## 📄 License
-
-Distributed under the MIT License. See `LICENSE` for more information.
-
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { createAuthMiddleware } from "better-auth/api";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -13,4 +14,31 @@ export const auth = betterAuth({
             prompt: "select_account",
         },
     },
+    hooks: {
+        after: createAuthMiddleware(async (ctx) => {
+            // create new workspace when the user has signed up
+            if (ctx.path.startsWith("/sign-up")) {
+                const newSession = ctx.context.newSession;
+                const userId = newSession?.user.id;
+                if (newSession && userId) {
+                    
+                    await prisma.$transaction(async (tx) => {
+                        const existingWorkspace = await tx.workspace.findFirst({
+                            where: {
+                                userId: userId
+                            }
+                        })
+                        if (!existingWorkspace) {
+                            await tx.workspace.create({
+                                data: {
+                                    name: "Workspace 1",
+                                    userId: userId
+                                }
+                            })
+                        }
+                    })
+                }
+            }
+        }),
+    }
 });
